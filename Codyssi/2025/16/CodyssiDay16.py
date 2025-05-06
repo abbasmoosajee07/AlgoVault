e549f20a10e6913cf418146842b2745827e83a8f
Abbas Moosajee <abbas07moosajee@gmail.com>
2025-05-06 18:34:52 +0100

    Codyssi Day 16

--- Codyssi/2025/16/CodyssiDay16.py
+++ Codyssi/2025/16/CodyssiDay16.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 start_time = time.time()
 
 # Load the input data from the specified file path
-D16_file = "Day16_input.txt"
+D16_file = "Day16_input1.txt"
 D16_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), D16_file)
 
 # Read and sort input data into a grid
@@ -24,8 +24,11 @@ with open(D16_file_path) as file:
     directions = input_data[1].split('\n')[0]
 
 class MindCube:
-    def __init__(self, cube_faces: list[str]):
-        cube_list = self.parse_input(cube_faces)
+    def __init__(self, cube_faces: list[str], size: tuple):
+        self.CUBE_SIZE = size
+        self.cube_face = self.parse_input(cube_faces)
+        self.base_cube = {(face, row, col): 1 for face in range(1, size[0] + 1)
+                        for row in range(size[1]) for col in range(size[2])}
 
     @staticmethod
     def parse_input(input_faces):
@@ -38,12 +41,47 @@ class MindCube:
                 cube_list.append((parts[0], int(parts[1]), int(parts[-1])))
         return cube_list
 
-    def perform_transformations(self, instructions):
-        return len(instructions)
+    def get_adjacent_face(self, current: int, twist: str):
+        
+        faces_dict = {
+            1:{"L":5, "R":6, "U":4, "D":2},
+            2:{"L":5, "R":6, "U":1, "D":3},
+            3:{"L":5, "R":6, "U":2, "D":4},
+            4:{"L":5, "R":6, "U":3, "D":1},
+            5:{"L":3, "R":1, "U":4, "D":2},
+            6:{"L":1, "R":3, "U":4, "D":2},
+        }
+            #   1
+            #  526
+            #   3
+            #   4
 
-cubes = MindCube(cube_faces)
+        new_face = faces_dict[current][twist]
+        return new_face
+
+    def perform_transformations(self, instructions):
+        current_face = 1
+        # for line in self.cube_face:
+        #     if line[0] == "FACE":
+        #         for row in range(self.CUBE_SIZE[1]):
+        #             for col in range(self.CUBE_SIZE[2]):
+        #                 self.base_cube[(current_face, row, col)] = line[1]
+        # print(self.base_cube)
+        for twist_dir in instructions:
+            twist_face = self.get_adjacent_face(current_face, twist_dir)
+            print(f"{twist_dir}: {current_face} -> {twist_face}")
+            current_face = twist_face
+        return len(self.base_cube)
+
+cubes = MindCube(cube_faces, (6, 80, 80))
 
 absorption = cubes.perform_transformations(directions)
 print("Part 1:", absorption)
 
 print(f"Execution Time = {time.time() - start_time:.5f}s")
+
+
+# The twist “L” is performed, the ‘current face’ changes to face 5. The orientation has changed due to the twist. At this stage, face 4 is above, face 1 is to the right, face 2 is below, and face 3 is to the left.
+# The twist “U” is performed, the ‘current face’ changes to face 4.
+# The twist “R” is performed, the ‘current face’ changes to face 1.
+# The twist “D” is performed, the ‘current face’ changes to face 5.
